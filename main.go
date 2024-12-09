527be3d401cb704c7c0dfb20506101b578f4da2d
Shane C <shane@scaffoe.com>
2024-12-08 20:29:37 -0500

    add update flag

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"egtyl.xyz/omnibill/archiver"
 	"egtyl.xyz/omnibill/tui"
 	"egtyl.xyz/omnibill/tui/progress"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -25,10 +26,12 @@ var homeDir = os.Getenv("HOME")
 
 var flagGrabChromeVersion bool
 var flagDirectory string
+var flagDoUpdate bool
 
 func init() {
 	flag.BoolVar(&flagGrabChromeVersion, "use-chrome-api", false, "uses the latest version of chrome from their API as the user agent.")
 	flag.StringVar(&flagDirectory, "directory", "", "the directory to download and extract the server to.")
+	flag.BoolVar(&flagDoUpdate, "update", false, "update mc bedrock server if new version is available.")
 	flag.Parse()
 
 	if len(flagDirectory) != 0 {
@@ -39,6 +42,15 @@ func init() {
 func main() {
 
 	fmt.Println(tui.Format(tui.FgColorGrey, tui.FmtBold) + "[ " + tui.Format(tui.FgColorGold, tui.FmtBoldReset) + "Bedrock Server Downloader " + VERSION + tui.Format(tui.FgColorGrey, tui.FmtBold) + " ]" + tui.FmtReset)
+
+	var currentVersion string
+	if flagDoUpdate {
+		curVersion, err := os.ReadFile(filepath.Join(homeDir, ".server_version"))
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			log.Fatalln(err)
+		}
+		currentVersion = string(curVersion)
+	}
 
 	cookieJar, err := cookiejar.New(nil)
 	if err != nil {
@@ -79,6 +91,11 @@ func main() {
 	gameVersion := mcVerInfo[1]
 	archiveDest := filepath.Join(homeDir, "bedrock_server_"+gameVersion+".zip")
 
+	if flagDoUpdate && currentVersion == gameVersion {
+		fmt.Println("No updates found.")
+		return
+	}
+
 	fmt.Println(tui.Format(tui.FgColorGrey, tui.FmtBold) + "[ " + tui.Format(tui.FgColorGold, tui.FmtBoldReset) + "Found Version: " + gameVersion + tui.Format(tui.FgColorGrey, tui.FmtBold) + " ]" + tui.FmtReset)
 
 	file, err := os.OpenFile(archiveDest, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
@@ -115,10 +132,21 @@ func main() {
 
 	fmt.Println(tui.Format(tui.FgColorGrey, tui.FmtBold) + "[ " + tui.Format(tui.FgColorGold, tui.FmtBoldReset) + "Extracting server archive" + tui.Format(tui.FgColorGrey, tui.FmtBold) + " ]" + tui.FmtReset)
 
-	if err := archiver.Extract(archiveDest, archiver.ExtractOptions{
+	extractOptions := archiver.ExtractOptions{
 		Overwrite: true,
 		Folder:    homeDir,
-	}); err != nil {
+	}
+
+	if flagDoUpdate && currentVersion != gameVersion {
+		// TODO: Replace with ExtractFile in archiver package when available.
+		extractOptions.Filter = regexp.MustCompile(`bedrock_server\b`)
+	}
+
+	if err := archiver.Extract(archiveDest, extractOptions); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := os.WriteFile(filepath.Join(homeDir, ".server_version"), []byte(gameVersion), 0600); err != nil {
 		log.Fatalln(err)
 	}
 
